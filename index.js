36a2648bcd18621bdcd3434947b350343f72f10f
Gerren <g.ballard23@outlook.com>
2021-07-15 14:10:08 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b){
+    return a + b
+
+}
+
+function subtract(a, b){
+ return a - b
+}
+
+function multiply(a, b){
+    return a * b
+
+}
+
+function divide(a, b){
+   return a / b
+}
+
+let number = 5;
+
+number += 3 //=> 8 
+
+number -= 2 //=> 6 
+
+number *= 10 //=> 60 
+
+number /= 5 //=> 12 
+
+ function increment(n){
+     return n += 1
+ }
+
+ function decrement(n){
+     return n -= 1
+ }
+
+ function makeInt(n){      
+    return parseInt(n, 10)
+ }
+
+ function preserveDecimal(n){
+     return parseFloat(n)
+ }
